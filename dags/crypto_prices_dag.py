24a2e4d1f9021bcfd2fe6ada61c841530facd9e4
Leandro Berto <leandroberto2010@gmail.com>
2026-05-14 20:31:05 -0300

    Divided dag in several tasks

--- dags/crypto_prices_dag.py
+++ dags/crypto_prices_dag.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from airflow.sdk import dag, task
-from src.run_crypto_prices_etl import run_crypto_prices_etl
+from src.extract.coingecko_client import fetch_crypto_markets
+from src.config import get_coingecko_config, get_database_config
+from src.transform.crypto_market import transform_market_data
+from src.load.postgres_loader import load_crypto_prices
+
 
 
 @dag(
@@ -15,11 +19,22 @@ def crypto_prices_etl_dag():
     """Define the Airflow workflow that runs the crypto prices ETL."""
 
     @task
-    def load_crypto_prices() -> int:
-        """Run the ETL pipeline and return the number of rows inserted."""
-        return run_crypto_prices_etl(per_page=100, page=1)
-
-    load_crypto_prices()
-
+    def extract_crypto_data():
+        """Extracts crypto data using coingecko"""
+        config = get_coingecko_config()
+        return fetch_crypto_markets(config)
+    @task()
+    def transform_crypto_data(raw_data):
+        """Transforms data and keeps only relevant information"""
+        return transform_market_data(raw_data)
+    @task()
+    def load_crypto_data(refined_data):
+        """Loads the refined and clean data into Postgres"""
+        dbconfig = get_database_config()
+        return load_crypto_prices(dbconfig, refined_data)
+    
+    raw_data = extract_crypto_data()
+    refined_data = transform_crypto_data(raw_data)
+    load_crypto_data(refined_data)
 
 crypto_prices_etl_dag()
